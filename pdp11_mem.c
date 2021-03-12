34503b7b8a65a39356c4a17ac9716ae54840833d
kirill0804 <morozov.ks@phystech.edu>
2021-03-12 22:35:09 +0300

    types, prototypes b/w r/w

---
+++ pdp11_mem.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+typedef  unsigned char  byte;    // 8 bit
+typedef  unsigned short int word;    // 16 bit
+typedef   word Adress;  // 16 bit
+
+
+
+#define MEMSIZE (64*1024)
+
+
+byte mem[MEMSIZE];
+
+void b_write(Adress adr, byte b);
+byte b_read(Adress adr);
+void w_write(Adress adr, word w);
+word w_read(Adress adr);
+
+
+int main() {
+	
+	return 0;
+}
